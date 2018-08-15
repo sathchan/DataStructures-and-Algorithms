360b55dc8ea080385aa01eeff0604a6a28b23baf
Sathish Chandran <sathchan@microsoft.com>
2018-08-15 11:52:30 +0530

    Algorithm - PartitionLabel

---
+++ algorithms/AlgorithmsLibrary/Medium/PartitionLabels.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsLibrary
+{
+    public class PartitionLabels
+    {
+        public List<int> Main(String S)
+        {
+            if (S == null || S.Length == 0)
+            {
+                return null;
+            }
+            List<int> list = new List<int>();
+            int[] map = new int[26];  
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                map[S[i] - 'a'] = i;
+            }
+            
+            int last = 0;
+            int start = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                last = Math.Max(last, map[S[i] - 'a']);
+                if (last == i)
+                {
+                    list.Add(last - start + 1);
+                    start = last + 1;
+                }
+            }
+
+            return list;
+        }
+    }
+}
--- algorithms/DSA/Program.cs
+++ algorithms/DSA/Program.cs
@@ -7,20 +7,9 @@ namespace DSA
     {
         static void Main(string[] args)
         {
-            ListNode l1 = new ListNode(1);
-            ListNode l2 = new ListNode(2);
-            ListNode l7 = new ListNode(6);
-            ListNode l3 = new ListNode(3);
-            ListNode l4 = new ListNode(4);
-            ListNode l5 = new ListNode(5);
-            ListNode l6 = new ListNode(6);
-            
 
-            l1.next = l2; l2.next = l7; l7.next = l3;
-            l3.next = l4; l4.next = l5; l5.next = l6;
-
-            RemoveLinkedElements obj = new RemoveLinkedElements();
-            var result = obj.Main(l1, 6);
+            PartitionLabels obj = new PartitionLabels();
+            var result = obj.Main("ababcbacadefegdehijhklij");
 
             Console.WriteLine(result.ToString());
             Console.ReadLine();
