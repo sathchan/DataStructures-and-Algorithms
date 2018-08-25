e74ed09d5132b30e90bb34c2911ae710d180fcef
Sathish Chandran <sathchan@microsoft.com>
2018-08-25 20:14:19 +0530

    algo: Single element in a sorted array

---
+++ algorithms/AlgorithmsLibrary/Medium/SingleNonDuplicate.cs
@@ -0,0 +1,35 @@
+﻿namespace AlgorithmsLibrary
+{
+    /*
+     
+        Single Element in a Sorted Array
+        ---------------------------------
+        Given a sorted array consisting of only integers where every element appears twice except for one element which appears once. Find this single element that appears only once.
+
+        Example 1:
+        Input: [1,1,2,3,3,4,4,8,8]
+        Output: 2
+
+        Example 2:
+        Input: [3,3,7,7,10,11,11]
+        Output: 10
+        Note: Your solution should run in O(log n) time and O(1) space.
+
+        540: https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+     */
+
+    public class SingleNonDuplicate
+    {
+        public int Main(int[] nums)
+        {
+            if (nums == null || nums.Length < 1) return 0;
+
+            for (int i = 0; i < nums.Length - 1; i += 2)
+            {
+                if (nums[i] != nums[i + 1]) return nums[i];
+            }
+
+            return nums[nums.Length - 1];
+        }
+    }
+}
