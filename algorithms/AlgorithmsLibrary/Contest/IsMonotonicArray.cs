339fc04675f7cdebd494bf56cf67424d70c584f4
Sathish Chandran <sathchan@microsoft.com>
2018-09-02 20:34:47 +0530

    algo: Monotonic array check

---
+++ algorithms/AlgorithmsLibrary/Contest/IsMonotonicArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsLibrary
+{
+    public class IsMonotonicArray
+    {
+        public bool IsMonotonic(int[] A)
+        {
+            if (!IsMonotonicIncreasing(A) && !IsMonotonicDecreasing(A)) return false;
+            return true;
+        }
+
+        public bool IsMonotonicIncreasing(int[] A)
+        {
+            int i = 0;
+            for (int j = 1; j < A.Length; j++)
+            {
+                if (A[i] <= A[j]) i++;
+                else return false;
+            }
+
+            return true;
+        }
+
+        public bool IsMonotonicDecreasing(int[] A)
+        {
+            int i = 0;
+            for (int j = 1; j < A.Length; j++)
+            {
+                if (A[i] >= A[j]) i++;
+                else return false;
+            }
+
+            return true;
+        }
+
+    }
+
+}
