e7bf484f47e86735d1b58aae1c91902759415eec
Sathish Chandran <sathchan@microsoft.com>
2018-09-10 04:13:43 +0530

    algo: RLE Iterator

---
+++ algorithms/AlgorithmsLibrary/Contest/RLEIterator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace AlgorithmsLibrary
+{
+    public class RLEIterator
+    {
+        int[] IteratedA = null; int nextIndex = 0;
+        public RLEIterator(int[] A)
+        {
+            List<int> tempIterator = new List<int>();
+            IteratedA = new int[A.Length];
+            for (int i = 0; i < A.Length; i += 2)
+            {
+                for (int j = 0; j < A[i]; j++)
+                {
+                    tempIterator.Add(A[i + 1]);
+                }
+            }
+
+            IteratedA = tempIterator.ToArray();
+        }
+
+        public int Next(int n)
+        {
+            int output = -1;
+            nextIndex += n;
+
+            if (IteratedA.Length < nextIndex)
+            {
+                IteratedA = new int[] { }; //Reset so next value is taken from the latest
+                return output;
+            }
+            else
+            {
+                return IteratedA[nextIndex -1];
+            }           
+        }
+    }
+}
